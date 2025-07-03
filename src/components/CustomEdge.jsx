02975479751c13b4ca0dd84ff100e75f263ee759
SriDevi <uniquesridevi0629@gmail.com>
2025-07-03 20:13:55 +0530

    Initial commit: Pipeline Editor DAG builder with React + Tailwind + React Flow

---
+++ src/components/CustomEdge.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import {
+  BaseEdge,
+  EdgeLabelRenderer,
+  getBezierPath,
+  useReactFlow,
+} from "reactflow";
+import crossImg from "../assets/cross.svg";
+
+const CustomEdge = ({
+  id,
+  sourceX,
+  sourceY,
+  targetX,
+  targetY,
+  targetPosition,
+  sourcePosition,
+}) => {
+  const { setEdges } = useReactFlow();
+  const handleEdgeDelete = () => {
+    setEdges((prevEdges) => prevEdges.filter((edge) => edge?.id !== id));
+  };
+  const [edgePath, labelX, labelY] = getBezierPath({
+    sourceX,
+    sourceY,
+    targetX,
+    targetY,
+    targetPosition,
+    sourcePosition,
+  });
+
+  return (
+    <>
+      <BaseEdge id={id} path={edgePath} />
+
+      <EdgeLabelRenderer>
+        <button
+          className="h-8 w-8 absolute opacity-30 hover:opacity-100 nodrag nopans"
+          style={{
+            transform: `translate(-50%, -50%) translate(${labelX}px,${labelY}px)`,
+            pointerEvents: "all",
+          }}
+          onClick={handleEdgeDelete}
+        >
+          <img
+            className="h-full w-full object-contain"
+            src={crossImg}
+            alt="cross-image"
+          />
+        </button>
+      </EdgeLabelRenderer>
+    </>
+  );
+};
+
+export default CustomEdge;
---
+++ src/utils/constants.js
@@ -0,0 +1,46 @@
+import { CustomEdge, DestinationNode, SourceNode } from "../components";
+
+export const initialNodes = [
+  {
+    id: "1",
+    position: { x: -600, y: 100 },
+    data: { title: "Source 1" },
+    type: "source-node",
+  },
+  {
+    id: "2",
+    position: { x: -600, y: 200 },
+    data: { title: "Source 2" },
+    type: "source-node",
+  },
+  {
+    id: "3",
+    position: { x: -600, y: 300 },
+    data: { title: "Source 3" },
+    type: "source-node",
+  },
+  {
+    id: "4",
+    position: { x: 600, y: 100 },
+    data: { title: "Destinaton 1" },
+    type: "destination-node",
+  },
+  {
+    id: "5",
+    position: { x: 600, y: 400 },
+    data: { title: "Destinaton 2" },
+    type: "destination-node",
+  },
+];
+
+export const initialEdges = [];
+
+export const nodeTypes = {
+  "source-node": SourceNode,
+  "destination-node": DestinationNode,
+};
+export const edgeTypes = {
+  "custom-edge": CustomEdge,
+};
+
+
